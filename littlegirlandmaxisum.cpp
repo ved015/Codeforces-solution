ff6803e52e84d6d6d450016462b1eb4a293b4608
ved015 <vedant.04.mahajan@gmail.com>
2025-01-12 12:01:02 +0530

    littlegirl

---
+++ littlegirlandmaxisum.cpp
@@ -0,0 +1,94 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll max(ll a, ll b, ll c){ return max(a, max(b, c));}
+ll min(ll a, ll b, ll c){ return min(a, min(b, c));}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){string str; while(num){if(num & 1) str.pb('1');else str.pb('0');num>>=1;}return str;}
+ll BinaryToDecimal(string num){ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) {if (num[i] == '1') ans += base; base<<=1;}return ans;}
+
+void Vedant(){
+    ll n,q;
+    cin >> n >> q;
+    vll a(n);
+    for(ll i = 0; i < n; i++) cin >> a[i];
+
+    sort(a.rbegin(),a.rend());
+    vector<pair<int, int> > queries(q);
+ 
+    for (int i = 0; i < q; i++)
+    {
+        cin >> queries[i].first >> queries[i].second;
+    }
+ 
+    vector<int> temp(n);
+ 
+    for (auto &it : queries)
+    {
+        int l = it.first - 1;
+        int r = it.second;
+ 
+        temp[l]++;
+        if (r <= n - 1)
+            temp[r]--;
+    }
+
+    vector<int> pref(n);
+    pref[0] = temp[0];
+    for (int i = 1; i < n; i++)
+    {
+        pref[i] = pref[i - 1] + temp[i];
+    }
+ 
+ 
+    priority_queue<pair<int, int > > pq;
+    for (int i = 0; i < n; i++)
+    {
+        pq.push(mp(pref[i], i));
+    }
+ 
+    ll sum = 0;
+    int i = 0;
+    while (!pq.empty())
+    {
+        int x = pq.top().first;
+        pq.pop();
+        sum += (x * a[i]);
+        i++;
+    }
+ 
+    cout << sum;
+}
+
+
+int main(){
+    fastio();
+    int t = 1;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}

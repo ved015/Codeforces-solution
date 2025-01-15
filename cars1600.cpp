f0317e350e8a3e6a9d9c3419dc1501eabdb6d4d6
ved015 <vedant.04.mahajan@gmail.com>
2025-01-15 20:53:37 +0530

    div2

---
+++ cars1600.cpp
@@ -0,0 +1,87 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll max(ll a, ll b, ll c){ return max(a, max(b, c));}
+ll min(ll a, ll b, ll c){ return min(a, min(b, c));}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){string str; while(num){if(num & 1) str.pb('1');else str.pb('0');num>>=1;}return str;}
+ll BinaryToDecimal(string num){ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) {if (num[i] == '1') ans += base; base<<=1;}return ans;}
+
+
+int findpos(int val,vi &temp){
+    for(int i = 0; i < sz(temp); i++){
+        if(temp[i] == val) return i;
+    }
+    return -1;
+}
+
+void generatemoves(vi &intial,vi &final,vector<pii> &moves){
+
+    for(int i = 0; i < sz(intial); i++){
+        if(intial[i] == final[i]) continue;
+        else{
+            int pos = findpos(final[i],intial);
+            for(int j = pos; j > i; j--){
+                moves.pb(mp(intial[j],intial[j - 1]));
+                swap(intial[j],intial[j - 1]);
+            }
+        }
+    }
+    return;
+} 
+
+
+void Vedant(){
+    int n;
+    cin >> n;
+    vi final(n);
+    for(int i = 0; i < n; i++) cin >> final[i];
+
+    vi temp = final;
+    reverse(all(temp));
+
+    vector<pii> moves;
+    vi intital;
+    for(int i = 1; i <= n; i++) intital.pb(i);
+
+    generatemoves(intital,temp,moves);
+    generatemoves(temp,final,moves);
+
+    cout << moves.size() << endl;
+    for(auto it : moves){
+        cout << it.first << " " << it.second << endl;
+    }
+    return;
+}
+
+
+int main(){
+    fastio();
+    int t = 1;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}
---
+++ perpendicularsegments.cpp
@@ -0,0 +1,61 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll max(ll a, ll b, ll c){ return max(a, max(b, c));}
+ll min(ll a, ll b, ll c){ return min(a, min(b, c));}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){string str; while(num){if(num & 1) str.pb('1');else str.pb('0');num>>=1;}return str;}
+ll BinaryToDecimal(string num){ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) {if (num[i] == '1') ans += base; base<<=1;}return ans;}
+
+void Vedant(){
+    int x,y,k;
+    cin >> x >> y >> k;
+    int ax,ay,bx,by,cx,cy,dx,dy;
+
+    ax = 0;
+    bx = min(x,y);
+    ay = 0;
+    by = min(x,y);
+    // length = root(x^2 + y^2) this is the max value and atq k is according to x,y
+    cx = 0;
+    dx = min(x,y);
+    cy = min(x,y);
+    dy = 0;
+
+    // len(AB) = len(CD) slope(AB) = y/x slope(CD) = -x/y
+    cout << ax << " " << ay << " " << bx << " " << by << endl;
+    cout << cx << " " << cy << " " << dx << " " << dy << endl;
+}
+
+
+int main(){
+    fastio();
+    int t;
+    cin >> t;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}

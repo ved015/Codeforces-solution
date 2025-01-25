09d3a148caeecdb84858443bf93be745f0fe4adf
ved015 <vedant.04.mahajan@gmail.com>
2025-01-25 12:49:04 +0530

    cses sorting

---
+++ moviescses.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int solve(){
+	ll n;
+	cin >> n;
+	vector<pair<ll, ll>> t(n);
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> t[i].first >> t[i].second;
+	}
+	sort(t.begin(),t.end());
+
+	ll ans = 0, current = 0;
+	for (ll i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			current = t[i].second;
+			ans++;
+		}
+		else
+		{
+			if (t[i].first >= current)
+			{
+				current = t[i].second;
+				ans++;
+			}
+			else
+			{
+				current = min(current, t[i].second);
+			}
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
+int main()
+{
+	int testCase=1;
+	while(testCase--){
+		solve();
+	}
+	return 0;
+}

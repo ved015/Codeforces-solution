f7a4f5cafe6a060096c6cad3ece4ed22f75880fe
ved015 <vedant.04.mahajan@gmail.com>
2025-01-26 17:46:42 +0530

    cses

---
+++ towerscses.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){string str; while(num){if(num & 1) str.pb('1');else str.pb('0');num>>=1;}return str;}
+ll BinaryToDecimal(string num){ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) {if (num[i] == '1') ans += base; base<<=1;}return ans;}
+
+void Vedant(){
+    int n;
+    cin >> n;
+    vector<pii> tower(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> tower[i].first;
+        tower[i].second = i;
+    }
+
+    sort(all(tower));
+    reverse(all(tower));
+
+    // for(auto it : tower){
+    //     cout << it.first << " " << it.second << endl;
+    // }
+
+    int i = 0;
+    int cnt = 0;
+    bool flag = false;
+
+    while(true){
+        while(i < n){
+            if(i == n - 1){
+                flag = true;
+                break;
+            }
+            if(tower[i].second < tower[i + 1].second){
+                i++;
+            }
+            else{
+                i++;
+                break;
+            }
+        }
+        // cout << i << endl;
+        cnt++;
+        if(flag) break;
+    }
+    cout << cnt;
+}
+
+
+int main(){
+    fastio();
+    int t = 1;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}

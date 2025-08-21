aad33dc4cb23cb525c9774c780ca109298900fed
ved015 <vedant.04.mahajan@gmail.com>
2025-08-21 19:47:33 +0530

    round 1021

---
+++ round1021/c.cpp
@@ -0,0 +1,99 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll gcd(ll a, ll b) {if (b > a) return gcd(b, a); if (b == 0) return a; return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){ string str; while(num){ if(num & 1) str.pb('1'); else str.pb('0'); num >>= 1; } return str; }
+ll BinaryToDecimal(string num){ ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) { if (num[i] == '1') ans += base; base <<= 1; } return ans; }
+
+long long pow(long long x,long long n){
+    long long ans = 1;
+    while(n > 0){
+        if(n & 1){
+            ans = ans * x;
+            n--;
+        }
+        else{
+            x = x * x;
+            n = n / 2;
+        }
+    }
+    return ans;
+}
+
+void Vedant(){
+    int n;
+    cin >> n;
+    vll a(n);
+    for(auto &it : a) cin >> it;
+    sort(all(a));
+
+    map<int,int> mp;
+
+    for(int i = 0; i < n; i++){
+        if(i == 0 || a[i] - a[i - 1] <= 1) mp[a[i]]++;
+        else{
+            int cnt = 0;
+            for(auto &it : mp){
+                if(it.second >= 4){
+                    cout << "YES" << endl;
+                    return;
+                }
+                else if(it.second >= 2) cnt++;
+            }
+            if(cnt >= 2){
+                cout << "YES" << endl;
+                return;
+            }
+            mp.clear();
+            mp[a[i]]++;
+        }
+    }
+    int cnt = 0;
+    for(auto &it : mp){
+        if(it.second >= 4){
+            cout << "YES" << endl;
+            return;
+        }
+        else if(it.second >= 2) cnt++;
+    }
+    if(cnt >= 2){
+        cout << "YES" << endl;
+        return;
+    }
+
+    cout << "NO" << endl;
+    return;
+}
+
+int main(){
+    fastio();
+    int t;
+    cin >> t;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}

afe08c9770814ea9531ac639773de5a7459440bc
ved015 <vedant.04.mahajan@gmail.com>
2025-08-22 22:47:03 +0530

    round1022

---
+++ round1022/c.cpp
@@ -0,0 +1,93 @@
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<ll> > vvll;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(),x.end()
+
+ll gcd(ll a, ll b) {if (b > a) return gcd(b, a); if (b == 0) return a; return gcd(b, a % b);}
+ll fact(ll a) { return (a <= 1) ? 1 : a * fact(a - 1); }
+
+vll factors(int n) { vll ans; int l = sqrt(n); for (int i = 1; i <= l; i++) { if (n % i == 0){ if (n/i == i) ans.pb(i); else {ans.pb(i); ans.pb(n/i);}}} return ans;}
+string DecimalToBinary(ll num){ string str; while(num){ if(num & 1) str.pb('1'); else str.pb('0'); num >>= 1; } return str; }
+ll BinaryToDecimal(string num){ ll ans = 0; ll base = 1; for (int i = sz(num) - 1; i >= 0; i--) { if (num[i] == '1') ans += base; base <<= 1; } return ans; }
+
+long long pow(long long x,long long n){
+    long long ans = 1;
+    while(n > 0){
+        if(n & 1){
+            ans = ans * x;
+            n--;
+        }
+        else{
+            x = x * x;
+            n = n / 2;
+        }
+    }
+    return ans;
+}
+
+void Vedant(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    } // n
+    vector<int> b;
+    int l = -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] != l)
+            b.push_back(a[i]);
+        l = a[i];
+    } // n
+    n = b.size();
+    vector<pair<int, int>> vec(n);
+    for (int i = 0; i < n; i++) {
+        vec[i] = {b[i], i};
+    } // n
+    sort(vec.rbegin(), vec.rend()); // nlogn
+ 
+    vector<int> chk(n, 0);
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int j = vec[i].second;
+        if (j > 0 && chk[j - 1]) {
+            chk[j] = 1;
+            continue;
+        }
+        if (j < n - 1 && chk[j + 1]) {
+            chk[j] = 1;
+            continue;
+        }
+        chk[j] = 1;
+        ans++;
+    } // n
+    cout << ans << '\n';
+}
+
+int main(){
+    fastio();
+    int t;
+    cin >> t;
+    while(t--){
+       Vedant();
+    }
+    return 0;
+}
